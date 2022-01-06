936e473f4d0a3438891c55b47cec1fcb1d95accc
Reese Kuper <reese.kuper@gmail.com>
2022-01-06 00:03:10 -0600

    proper data and scratch paths

--- configs.sh
+++ configs.sh
@@ -1,9 +1,12 @@
+
+TAILBENCH_PATH=$HOME/benchmarks/tailbench
+
 # Set this to point to the top level of the TailBench data directory
-DATA_ROOT=$HOME/benchmarks/tailbench.inputs
+DATA_ROOT=$TAILBENCH_PATH/data
 
 # Set this to point to the top level installation directory of the Java
 # Development Kit. Only needed for Specjbb
 JDK_PATH=/usr/lib/jvm/java-8-openjdk-amd64/
 
 # This location is used by applications to store scratch data during execution.
-SCRATCH_DIR=$HOME/benchmarks/tailbench/scratch
+SCRATCH_DIR=$TAILBENCH_PATH/scratch
