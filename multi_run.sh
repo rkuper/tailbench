35cfe6c86ea9be45d5ef96cd0811de64e773b573
Reese Kuper <reese.kuper@gmail.com>
2022-01-10 01:44:03 -0800

    added options to multi run for just processing and limited printed context

--- multi_run.sh
+++ multi_run.sh
@@ -23,6 +23,14 @@ while [[ $# -gt 0 ]]; do
       KEEP_LOGS="$2"
       shift
       ;;
+    -c|--context)
+      KEEP_LOGS="$2"
+      shift
+      ;;
+    -e|--run_benchmarks)
+      KEEP_LOGS="$2"
+      shift
+      ;;
     *)
       POSITIONAL+=("$1")
       shift
@@ -36,6 +44,8 @@ BENCHMARKS=($BENCHMARKS)
 REGEX_NUM='^[0-9]+$'
 TOTAL_RUNS=$([[ -v TOTAL_RUNS && $TOTAL_RUNS =~ $REGEX_NUM ]] && echo "$TOTAL_RUNS" || echo "1")
 KEEP_LOGS=$([ -v KEEP_LOGS -o -z KEEP_LOGS ] && echo "true" || echo "false")
+CONTEXT=$([ -v CONTEXT -o -z CONTEXT ] && echo "true" || echo "false")
+RUN_BENCHMARKS=$([ -v RUN_BENCHMARKS -o -z RUN_BENCHMARKS ] && echo "true" || echo "false")
 OUTPUT_FILE=$([ -v OUTPUT_FILE ] && echo "$OUTPUT_FILE" || echo "results.txt")
 declare -a CONFIGS=("integrated" "networked")
 declare -a TYPES=("Queue" "Service" "Sojourn")
@@ -46,55 +56,58 @@ declare -a METRICS=("50th" "75th" "90th" "95th" "99th" "99.5th" "Mean" "Max")
 ####################################
 #            Benchmarks            #
 ####################################
-for BENCHMARK in "${BENCHMARKS[@]}"
-do
-  echo "##########################"
-  echo "#        ${BENCHMARK}         #"
-  echo "##########################"
-  echo ""
-
-  cd $BENCHMARK
-  for CONFIG in "${CONFIGS[@]}"
+if [ "$RUN_BENCHMARKS" == "true" ]
+then
+  for BENCHMARK in "${BENCHMARKS[@]}"
   do
-    case $CONFIG in
-      integrated)
-        for RUN in $(eval echo {1..$TOTAL_RUNS})
-        do
-          echo "Run Integrated ${RUN}:"
-          echo "================="
-          sudo ./run.sh
-          if [ -f "lats.bin" ];
-          then
-            python3 ../utilities/parselats.py lats.bin > ${BENCHMARK}_${CONFIG}_${RUN}_lats.txt
-            mv ${BENCHMARK}_${CONFIG}_${RUN}_lats.txt ../lats
-            rm lats.bin
-          fi
-          echo ""
-        done
-        ;;
-      networked)
-        for RUN in $(eval echo {1..$TOTAL_RUNS})
-        do
-          echo "Run Networked ${RUN}:"
-          echo "================"
-          sudo ./run_networked.sh
-          if [ -f "lats.bin" ];
-          then
-            python3 ../utilities/parselats.py lats.bin > ${BENCHMARK}_${CONFIG}_${RUN}_lats.txt
-            mv ${BENCHMARK}_${CONFIG}_${RUN}_lats.txt ../lats
-            rm lats.bin
-          fi
-          echo ""
-        done
-        ;;
-      *)
-        break
-        ;;
-    esac
+    echo "##########################"
+    echo "#        ${BENCHMARK}         #"
+    echo "##########################"
+    echo ""
+
+    cd $BENCHMARK
+    for CONFIG in "${CONFIGS[@]}"
+    do
+      case $CONFIG in
+        integrated)
+          for RUN in $(eval echo {1..$TOTAL_RUNS})
+          do
+            echo "Run Integrated ${RUN}:"
+            echo "================="
+            sudo ./run.sh
+            if [ -f "lats.bin" ];
+            then
+              python3 ../utilities/parselats.py lats.bin > ${BENCHMARK}_${CONFIG}_${RUN}_lats.txt
+              mv ${BENCHMARK}_${CONFIG}_${RUN}_lats.txt ../lats
+              rm lats.bin
+            fi
+            echo ""
+          done
+          ;;
+        networked)
+          for RUN in $(eval echo {1..$TOTAL_RUNS})
+          do
+            echo "Run Networked ${RUN}:"
+            echo "================"
+            sudo ./run_networked.sh
+            if [ -f "lats.bin" ];
+            then
+              python3 ../utilities/parselats.py lats.bin > ${BENCHMARK}_${CONFIG}_${RUN}_lats.txt
+              mv ${BENCHMARK}_${CONFIG}_${RUN}_lats.txt ../lats
+              rm lats.bin
+            fi
+            echo ""
+          done
+          ;;
+        *)
+          break
+          ;;
+      esac
+    done
+    cd ..
+    echo ""; echo ""
   done
-  cd ..
-  echo ""; echo ""
-done
+fi
 
 
 
@@ -119,10 +132,16 @@ echo "Porcessing..."
         do
           if [ -f "${BENCHMARK}_${CONFIG}_1_lats.txt" ];
           then
-            echo "[$TYPE] $METRIC: " \
-              `awk '/\['"$TYPE"'\] '"$METRIC"'/ {sum += $5; n++} END { if (n > 0) print sum / n; print "ms" }' \
-              ${BENCHMARK}_${CONFIG}_*_lats.txt`
-          fi
+						if [ "$CONTEXT" == "true" ]
+						then
+            	echo "[$TYPE] $METRIC: " \
+              	`awk '/\['"$TYPE"'\] '"$METRIC"'/ {sum += $5; n++} END { if (n > 0) print sum / n; print "ms" }' \
+              	${BENCHMARK}_${CONFIG}_*_lats.txt`
+						else
+            	echo `awk '/\['"$TYPE"'\] '"$METRIC"'/ {sum += $5; n++} END { if (n > 0) print sum / n }' \
+              	${BENCHMARK}_${CONFIG}_*_lats.txt`
+          	fi
+					fi
         done
       echo ""
       done
