ccf7f9dd8359be0d84e8ad8bcd87b6b032f7e095
Reese Kuper <reese.kuper@gmail.com>
2022-01-13 23:17:34 -0800

    Better script with more options and metrics tested. Added testing DRAM and PMM modes, all in one

--- multi_run.sh
+++ multi_run.sh
@@ -24,11 +24,15 @@ while [[ $# -gt 0 ]]; do
       shift
       ;;
     -c|--context)
-      KEEP_LOGS="$2"
+      CONTEXT="$2"
       shift
       ;;
     -e|--run_benchmarks)
-      KEEP_LOGS="$2"
+      RUN_BENCHMARKS="$2"
+      shift
+      ;;
+    -p|--process)
+      PROCESS="$2"
       shift
       ;;
     *)
@@ -39,17 +43,20 @@ while [[ $# -gt 0 ]]; do
 done
 
 BENCHMARKS=$([ "$BENCHMARKS" == "all" -o -z "${BENCHMARKS+x}" -o "$BENCHMARKS" == "" ] \
-  && echo "img-dnn masstree moses silo sphinx specjbb xapian" || echo "$BENCHMARKS")
+  && echo "img-dnn masstree moses silo specjbb sphinx xapian" || echo "$BENCHMARKS")
 BENCHMARKS=($BENCHMARKS)
 REGEX_NUM='^[0-9]+$'
 TOTAL_RUNS=$([[ -v TOTAL_RUNS && $TOTAL_RUNS =~ $REGEX_NUM ]] && echo "$TOTAL_RUNS" || echo "1")
 KEEP_LOGS=$([ -v KEEP_LOGS -o -z KEEP_LOGS ] && echo "true" || echo "false")
 CONTEXT=$([ -v CONTEXT -o -z CONTEXT ] && echo "true" || echo "false")
 RUN_BENCHMARKS=$([ -v RUN_BENCHMARKS -o -z RUN_BENCHMARKS ] && echo "true" || echo "false")
+PROCESS=$([ -v PROCESS -o -z PROCESS ] && echo "true" || echo "false")
 OUTPUT_FILE=$([ -v OUTPUT_FILE ] && echo "$OUTPUT_FILE" || echo "results.txt")
-declare -a CONFIGS=("integrated" "networked")
+OUTPUT_PATH=`pwd`/results/${OUTPUT_FILE}
+declare -a BENCH_CONFIGS=("integrated" "networked")
+declare -a MEM_CONFIGS=("dram" "pmem")
 declare -a TYPES=("Queue" "Service" "Sojourn")
-declare -a METRICS=("50th" "75th" "90th" "95th" "99th" "99.5th" "Mean" "Max")
+declare -a TAIL_METRICS=("50th" "75th" "90th" "95th" "99th" "99.5th" "Mean" "Max")
 
 
 
@@ -66,44 +73,34 @@ then
     echo ""
 
     cd $BENCHMARK
-    for CONFIG in "${CONFIGS[@]}"
+
+    for MEM_CONFIG in "${MEM_CONFIGS[@]}"
     do
-      case $CONFIG in
-        integrated)
-          for RUN in $(eval echo {1..$TOTAL_RUNS})
-          do
-            echo "Run Integrated ${RUN}:"
-            echo "================="
-            sudo ./run.sh
-            if [ -f "lats.bin" ];
-            then
-              python3 ../utilities/parselats.py lats.bin > ${BENCHMARK}_${CONFIG}_${RUN}_lats.txt
-              mv ${BENCHMARK}_${CONFIG}_${RUN}_lats.txt ../lats
-              rm lats.bin
-            fi
-            echo ""
-          done
-          ;;
-        networked)
-          for RUN in $(eval echo {1..$TOTAL_RUNS})
-          do
-            echo "Run Networked ${RUN}:"
-            echo "================"
-            sudo ./run_networked.sh
-            if [ -f "lats.bin" ];
-            then
-              python3 ../utilities/parselats.py lats.bin > ${BENCHMARK}_${CONFIG}_${RUN}_lats.txt
-              mv ${BENCHMARK}_${CONFIG}_${RUN}_lats.txt ../lats
-              rm lats.bin
-            fi
-            echo ""
-          done
-          ;;
-        *)
-          break
-          ;;
-      esac
+
+      for BENCH_CONFIG in "${BENCH_CONFIGS[@]}"
+      do
+
+        for RUN in $(eval echo {1..$TOTAL_RUNS})
+        do
+
+          echo "Run ${BENCH_CONFIG} ${RUN}:"
+          echo "================="
+          sudo pcm --external_program sudo pcm-memory --external_program \
+            sudo numactl --cpunodebind=0 --membind=$([ "$MEM_CONFIG" == "dram" ] && echo "0" || echo "2") \
+            ./run_${BENCH_CONFIG}.sh > ${BENCHMARK}_${MEM_CONFIG}_${BENCH_CONFIG}_${RUN}_pcm.txt
+          if [ -f "lats.bin" ];
+          then
+            python3 ../utilities/parselats.py lats.bin > ${BENCHMARK}_${MEM_CONFIG}_${BENCH_CONFIG}_${RUN}_lats.txt
+            mv ${BENCHMARK}_${MEM_CONFIG}_${BENCH_CONFIG}_${RUN}_*.txt ../results/${BENCHMARK}
+            rm lats.bin
+          fi
+          echo ""
+        done
+
+      done
+
     done
+
     cd ..
     echo ""; echo ""
   done
@@ -114,43 +111,105 @@ fi
 ####################################
 #            Processing            #
 ####################################
-echo "Porcessing..."
-{
-  cd lats
-  for BENCHMARK in "${BENCHMARKS[@]}"
-  do
-    echo "##########################"
-    echo "#        ${BENCHMARK}         #"
-    echo "##########################"
-    echo ""
-    for CONFIG in "${CONFIGS[@]}"
+if [ "$PROCESS" == "true" ]
+then
+ echo "Porcessing..."
+  {
+    cd results
+    for BENCHMARK in "${BENCHMARKS[@]}"
     do
-      echo "$CONFIG"; echo "=========="
-      for TYPE in "${TYPES[@]}"
-      do
-        for METRIC in "${METRICS[@]}"
-        do
-          if [ -f "${BENCHMARK}_${CONFIG}_1_lats.txt" ];
-          then
-						if [ "$CONTEXT" == "true" ]
-						then
-            	echo "[$TYPE] $METRIC: " \
-              	`awk '/\['"$TYPE"'\] '"$METRIC"'/ {sum += $5; n++} END { if (n > 0) print sum / n; print "ms" }' \
-              	${BENCHMARK}_${CONFIG}_*_lats.txt`
-						else
-            	echo `awk '/\['"$TYPE"'\] '"$METRIC"'/ {sum += $5; n++} END { if (n > 0) print sum / n }' \
-              	${BENCHMARK}_${CONFIG}_*_lats.txt`
-          	fi
-					fi
-        done
+
+      echo "##########################"
+      echo "#        ${BENCHMARK}         #"
+      echo "##########################"
       echo ""
+      cd $BENCHMARK
+
+      for MEM_CONFIG in "${MEM_CONFIGS[@]}"
+      do
+
+        for BENCH_CONFIG in "${BENCH_CONFIGS[@]}"
+        do
+
+          echo "$MEM_CONFIG $BENCH_CONFIG"; echo "==============="
+
+          # Metrics for tail latency reported by tailbench's harness
+          ##########################################################
+          for TYPE in "${TYPES[@]}"
+          do
+           if [ "$CONTEXT" == "false" ]
+            then
+              echo "$TYPE:"
+            fi
+            for TAIL_METRIC in "${TAIL_METRICS[@]}"
+            do
+              if [ -f "${BENCHMARK}_${MEM_CONFIG}_${BENCH_CONFIG}_1_lats.txt" ];
+              then
+                if [ "$CONTEXT" == "true" ]
+                then
+                  echo "[$TYPE] $TAIL_METRIC: " \
+                    `awk '/\['"$TYPE"'\] '"$TAIL_METRIC"'/ {sum += $5; n++} END { if (n > 0) print sum / n; print "ms" }' \
+                    ${BENCHMARK}_${MEM_CONFIG}_${BENCH_CONFIG}_*_lats.txt`
+                else
+                  echo `awk '/\['"$TYPE"'\] '"$TAIL_METRIC"'/ {sum += $5; n++} END { if (n > 0) print sum / n }' \
+                    ${BENCHMARK}_${MEM_CONFIG}_${BENCH_CONFIG}_*_lats.txt`
+                fi
+              fi
+            done
+          echo ""
+          done
+
+          # PCM metrics
+          #############
+          MEM_CONFIG_SEARCH=$([ "$MEM_CONFIG" == "dram" ] && echo "DRAM" || echo "PMM")
+          if [ -f "${BENCHMARK}_${MEM_CONFIG}_${BENCH_CONFIG}_1_pcm.txt" ];
+          then
+            if [ "$CONTEXT" == "true" ]
+            then
+              echo "[OVERALL] LLCRDMISSLAT: " \
+                `awk '/LLCRDMISSLAT / {getline; getline; sum += $11; n++} END { if (n > 0) print sum / n; print "ns"}' \
+                ${BENCHMARK}_${MEM_CONFIG}_${BENCH_CONFIG}_*_pcm.txt`
+
+              echo "[OVERALL] DIMM Energy: " \
+                `awk '/LLCRDMISSLAT / {getline; getline; sum += $10; n++} END { if (n > 0) print sum / n; }' \
+                ${BENCHMARK}_${MEM_CONFIG}_${BENCH_CONFIG}_*_pcm.txt`
+
+              echo "[OVERALL] ${MEM_CONFIG_SEARCH} Read Throughput: "\
+                `awk '/'"$MEM_CONFIG_SEARCH"' Read Throughput/ {sum += $6; n++} END { if (n > 0) print sum / n; print "MB/s"}' \
+                ${BENCHMARK}_${MEM_CONFIG}_${BENCH_CONFIG}_*_pcm.txt`
+
+              echo "[OVERALL] ${MEM_CONFIG_SEARCH} Write Throughput: "\
+                `awk '/'"$MEM_CONFIG_SEARCH"' Write Throughput/ {sum += $6; n++} END { if (n > 0) print sum / n; print "MB/s"}' \
+                ${BENCHMARK}_${MEM_CONFIG}_${BENCH_CONFIG}_*_pcm.txt`
+
+            else
+              echo "Overall:"
+              echo `awk '/LLCRDMISSLAT / {getline; getline; sum += $11; n++} END { if (n > 0) print sum / n; }' \
+                ${BENCHMARK}_${MEM_CONFIG}_${BENCH_CONFIG}_*_pcm.txt`
+
+              echo `awk '/DIMM energy / {getline; getline; sum += $10; n++} END { if (n > 0) print sum / n; }' \
+                ${BENCHMARK}_${MEM_CONFIG}_${BENCH_CONFIG}_*_pcm.txt`
+
+              echo `awk '/'"$MEM_CONFIG_SEARCH"' Read Throughput/ {sum += $6; n++} END { if (n > 0) print sum / n; }' \
+                ${BENCHMARK}_${MEM_CONFIG}_${BENCH_CONFIG}_*_pcm.txt`
+
+              echo `awk '/'"$MEM_CONFIG_SEARCH"' Write Throughput/ {sum += $6; n++} END { if (n > 0) print sum / n; }' \
+                ${BENCHMARK}_${MEM_CONFIG}_${BENCH_CONFIG}_*_pcm.txt`
+            fi
+          fi
+          echo ""
+
+        done
+        echo ""; echo "";
+
       done
+      cd ..
+
     done
-    echo ""; echo ""; echo ""
-  done
-  if [ "$KEEP_LOGS" == "false" ]
-  then
-    rm "$BENCHMARK"_*_lats_*.txt
-  fi
-  cd ..
-} > results.txt
+    if [ "$KEEP_LOGS" == "false" ]
+    then
+      rm "$BENCHMARK"_*_lats_*.txt
+    fi
+    cd ..
+  } > ${OUTPUT_PATH}
+fi
