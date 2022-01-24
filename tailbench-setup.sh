6d86e0e400b4f0a2c55b47821d40a9576995d9e4
Reese Kuper <reese.kuper@gmail.com>
2022-01-23 19:40:04 -0800

    minor testing automation updates

--- tailbench-setup.sh
+++
@@ -1,5 +0,0 @@
-# General
-sudo apt install g++ subversion automake libtool python-dev python2.7-dev make cmake openjdk-8-jdk
-
-# Benchmarks
-sudo apt install -y zlib1g-dev g++ libopencv-dev libboost-dev swift libicu-dev libboost-all-dev libbz2-dev liblzma-dev graphviz imagemagick libgoogle-perftools-dev ant libxapian-dev uuid-dev libjemalloc-dev libnuma-dev libdb-dev libdb++-dev libaio-dev libssl-dev swig bison libreadline-dev libgtop2-dev libncurses-dev libpulse-dev
--- xapian/build.sh
+++ xapian/build.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 # Install Xapian core if not already present
-if [[ ! -d xapian-core-1.2.13 ]]
+if [ ! -d "xapian-core-1.2.13" ]
 then
     tar -xf xapian-core-1.2.13.tar.gz
     cd xapian-core-1.2.13
